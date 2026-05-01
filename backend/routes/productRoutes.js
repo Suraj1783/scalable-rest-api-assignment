41abf1f45a06f9064cb987099713eca0ccf035b9
Suraj1783 <amrita00052@gmail.com>
2026-05-01 18:25:30 +0530

    Product CRUD APIs added

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createProduct, getProducts } from "../controllers/productController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, createProduct);
+router.get("/", protect, getProducts);
+
+export default router;
